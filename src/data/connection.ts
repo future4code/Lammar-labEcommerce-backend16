9e9dfeeb17eeb63e60f897d697c00236ceb0d901
Henrique Douglas Araujo <araujohdouglas@gmail.com>
2022-12-04 14:49:11 -0300

    Ajustes

--- src/data/connection.ts
+++ src/data/connection.ts
@@ -7,10 +7,10 @@ export const connection = knex({
 	client: "mysql",
 	connection: {
         host: process.env.DB_HOST,
-        port: 3306,
         user: process.env.DB_USER,
         password: process.env.DB_PASS,
         database: process.env.DB_NAME,
+        port: Number(process.env.PORT),
         multipleStatements:true
     }
 })
